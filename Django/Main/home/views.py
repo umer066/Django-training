2d98077cd63437a6fe4d50ed9f7f6e11d552adc9
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-01-25 00:33:10 +0500

    changes

--- Django/Main/home/views.py
+++ Django/Main/home/views.py
@@ -5,13 +5,20 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render (request,'index.html')
+
     # return HttpResponse ("This is Home-page")
 
 def about(request):
-    return HttpResponse ("This is About-page")
+    return render (request,'about.html')
+
+    # return HttpResponse ("This is About-page")
 
 def services(request):
-    return HttpResponse ("This is Services-page")
+    return render (request,'services.html')
+
+    # return HttpResponse ("This is Services-page")
 
 def contact(request):
-    return HttpResponse ("This is Contact-page")
+    return render (request,'contact.html')
+
+    # return HttpResponse ("This is Contact-page")
