7e93343709f6d091de5c3921fa86a517b79c4ec2
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-01-25 01:08:14 +0500

    Add django project

--- Django/Main/home/views.py
+++ Django/Main/home/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return render (request,'index.html')
+    return render (request,'base.html',)
 
     # return HttpResponse ("This is Home-page")
 
 def about(request):
-    return render (request,'about.html')
+    return render (request,'about.html',)
 
     # return HttpResponse ("This is About-page")
 
 def services(request):
-    return render (request,'services.html')
+    return render (request,'services.html',)
 
     # return HttpResponse ("This is Services-page")
 
 def contact(request):
-    return render (request,'contact.html')
+    return render (request,'contact.html',)
 
     # return HttpResponse ("This is Contact-page")
