d58ed0dd2dc90cd29d8ad204671805fe029644da
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-01-28 02:04:19 +0500

    Add django project

--- Django/Main/home/views.py
+++ Django/Main/home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 def index(request):
+    # messages.success(request,"This is a test message.")
     return render (request,'index.html',)
 
     # return HttpResponse ("This is Home-page")
@@ -19,6 +24,15 @@ def services(request):
     # return HttpResponse ("This is Services-page")
 
 def contact(request):
-    return render (request,'contact.html',)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        description = request.POST.get('description')
+        contact = Contact( name=name, email=email, phone=phone, description=description, date = datetime.today())
+        contact.save()
+
+        messages.success(request, "Your messages has been sent!")
+    return render (request,'contact.html')
 
     # return HttpResponse ("This is Contact-page")
