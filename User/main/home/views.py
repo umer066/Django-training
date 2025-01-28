26db0b7a17eb87a8464b29b01ac7f3b6b2221e04
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-01-28 22:11:35 +0500

    User login

---
+++ User/main/home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
+# Create your views here.
+
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request,'index.html')
+ 
+
+def login(request):
+
+    if request.method =="POST":
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        #  check if the user enter correct credentials
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            return redirect("/")
+        else:
+            # No backend authenticated the credentials
+            return render(request,'login.html')
+    
+     
+    return render(request,'login.html')
+
+
+def logout(request):
+    logout(request)
+    return redirect("/login")
